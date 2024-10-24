a98c346acbdece6ec235e651dd6d5e76e45ef7e5
jinsung <hyundil17@naver.com>
2024-10-25 01:59:58 +0900

    차트 구현 o 막대,캔들 모양바꾸기시도

--- stockchart/src/main/java/com/jucha/stockchart/StockhtmlController.java
+++ stockchart/src/main/java/com/jucha/stockchart/StockhtmlController.java
@@ -1,16 +1,27 @@
 package com.jucha.stockchart;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/api/stock-data")
 public class StockhtmlController {
 
-    @GetMapping("/stock-chart/{ticker}")
-    public String showStockChart(@PathVariable String ticker, Model model) {
-        model.addAttribute("ticker", ticker);  // 티커를 HTML로 전달
-        return "char";  // stockChart.html 파일을 반환
-    }
+	@Autowired
+	private StockDataService stockDataService;
+
+	@GetMapping("/{ticker}")
+	public String showStockChart(@PathVariable String ticker, Model model) {
+		List<Stock_Data> stockDataList = stockDataService.getStockDataByTicker(ticker);
+
+		model.addAttribute("stockDataList", stockDataList);
+		model.addAttribute("ticker", ticker);
+		return "char";
+	}
 }
