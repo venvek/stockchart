20c1618e86a4f2360a10d25f529335e863f5f5aa
jinsung <hyundil17@naver.com>
2024-11-15 23:18:15 +0900

    헤더 수정

--- stockchart/src/main/java/com/jucha/stockchart/StockhtmlController.java
+++ stockchart/src/main/java/com/jucha/stockchart/StockhtmlController.java
@@ -17,14 +17,6 @@ public class StockhtmlController {
 	@Autowired
 	private StockDataService stockDataService;
 
-	@GetMapping("/{ticker}")
-	public String showStockChart(@PathVariable String ticker, Model model) {
-		List<Stock_Data> stockDataList = stockDataService.getStockDataByTicker(ticker);
-
-		model.addAttribute("stockDataList", stockDataList);
-		model.addAttribute("ticker", ticker);
-		return "char";
-	}
 	
 	@GetMapping("/candle/{ticker}")
 	public String showStockcandleChart(@PathVariable String ticker, Model model) {
