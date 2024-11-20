3f4aaf994dcfc0cf81c1f311015d19c6564a7e69
jinsung <hyundil17@naver.com>
2024-11-20 23:26:03 +0900

    su cont

--- stockchart/src/main/java/com/jucha/stockchart/SuggestController.java
+++ stockchart/src/main/java/com/jucha/stockchart/SuggestController.java
@@ -1,5 +1,21 @@
 package com.jucha.stockchart;
 
-public class SuggestController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SuggestController {
+	
+	@Autowired
+	private CompanyService companyService;
+
+    @GetMapping("/suggestions")
+    public List<String> getSuggestions(@RequestParam String ticker) {
+        // 입력된 검색어로 필터링된 결과 반환
+        return companyService.getSuggetsions(ticker);
+    }
 }
