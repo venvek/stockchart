0659c68884ffc122c7593f5696bc400e11c82fc9
jinsung <hyundil17@naver.com>
2025-02-24 23:35:12 +0900

    query

--- stockchart/src/main/java/com/jucha/stockchart/CompanyRepository.java
+++ stockchart/src/main/java/com/jucha/stockchart/CompanyRepository.java
@@ -14,5 +14,9 @@ public interface CompanyRepository extends JpaRepository<Company, Long> {
     @Query("SELECT c.ticker FROM Company c WHERE c.ticker LIKE %:query% OR c.name LIKE %:query%")
     List<String> findTickersByQuery(@Param("query") String query);
     
+    @Query("SELECT c.ticker, c.name, s.close, s.open FROM Company c " +
+    	       "JOIN StockData s ON c.ticker = s.ticker " +
+    	       "WHERE s.date = (SELECT MAX(s2.date) FROM StockData s2 WHERE s2.ticker = c.ticker)")
+    	List<Object[]> findLatestStockData();
 
 }
