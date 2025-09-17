782f38bf6970ae50225051952eb1a855fe0923dc
jinsung <hyundil17@naver.com>
2025-09-17 23:52:53 +0900

    security 잠시

--- stockchart/src/main/java/config/SecurityConfig.java
+++ stockchart/src/main/java/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -11,18 +12,28 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+	@Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers( "/css/**","/login", "/js/**", "/images/**").permitAll() // 홈페이지/정적 리소스 누구나 접근
-                .anyRequest().authenticated() // 나머지는 로그인 필요
+                .anyRequest().authenticated() // 모든 요청은 인증 필요
             )
-            .oauth2Login(oauth -> oauth
-                .loginPage("/login")
-                .defaultSuccessUrl("/home", true) // ✅ 로그인 성공 후 홈페이지로 이동
-            );
+            // ✅ 스프링 제공 기본 로그인 폼 사용
+            .formLogin(Customizer.withDefaults())
+            .logout(Customizer.withDefaults()); 
 
         return http.build();
+    
+
+        
+		/*
+		 * http .authorizeHttpRequests(auth -> auth .requestMatchers("/", "/login",
+		 * "/css/**", "/js/**").permitAll() .anyRequest().authenticated() ) // ✅ 일반 로그인
+		 * 폼 활성화 .formLogin(form -> form .loginPage("/login") // 커스텀 로그인 페이지 URL
+		 * .permitAll() ) // ✅ 소셜 로그인 활성화 .oauth2Login(oauth2 -> oauth2
+		 * .loginPage("/login") ) .logout(logout -> logout.permitAll());
+		 * 
+		 * return http.build(); }
+		 */
     }
 }
